e71b3b3b0c904721e8ddbdcc4e35c9f724a288ca
85Luckyluck <lukaszhytros@interia.eu>
2022-04-24 20:07:56 +0200

    usuniecie home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageModule } from './pages/home.page-module';
+
 
 const routes: Routes = [{ 
-        path: 'home', 
-        loadChildren: () => HomePageModule
+        path: '', 
+        
       }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes),HomePageModule],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
