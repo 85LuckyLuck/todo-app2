f1ad83e6c0f700f2a43df5d8b87cfd94266a38a0
85Luckyluck <lukaszhytros@interia.eu>
2022-04-24 15:59:47 +0200

    instalacja firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'todoapp-ef21f',
+    appId: '1:705787354243:web:8306f920ff47eea6968fb6',
+    storageBucket: 'todoapp-ef21f.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDcNuyniEQbPsLejhFHkTNcqU6TgFCG6ys',
+    authDomain: 'todoapp-ef21f.firebaseapp.com',
+    messagingSenderId: '705787354243',
+  },
   production: false
 };
 
