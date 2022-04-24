e9f4029205c0b66be0c3ce10c3ef5a1e9eca3a87
85Luckyluck <lukaszhytros@interia.eu>
2022-04-24 21:30:23 +0200

    zrobienie todos component i dodanie formularza

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodosComponent } from './todos/todos.component';
 
 
 const routes: Routes = [{ 
-        path: '', 
+        path: '', component: TodosComponent
         
       }];
 
