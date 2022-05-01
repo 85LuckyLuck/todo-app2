2f604a5ab7425e576ddbad257f7df2d9347bf9c9
85Luckyluck <lukaszhytros@interia.eu>
2022-05-01 21:03:32 +0200

    create data-picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { DataPickerComponentModule } from '@todo2';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
     AppComponent,
   ],
   imports: [
+    DataPickerComponentModule,
     BrowserModule,
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
